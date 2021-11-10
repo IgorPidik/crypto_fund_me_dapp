4c571d2cf08afb70054e02831ba0604405c3a9b1
Igor Pidik <igorPidik@gmail.com>
2021-11-10 22:14:56 +0100

    card title

--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -8,15 +8,16 @@ function ProjectView(props) {
         <div className={'col-3'}>
             <div className="card text-white bg-dark mb-3">
                 <div className="card-body">
-                    <h5 className="card-title">Dark card title</h5>
+                    <h5 className="card-title text-center">Project #{props.project.id.toString()}: {props.project.name}</h5>
                     <div className="card-text">
-                        <p>{props.project.id.toString()}</p>
-                        <p>{props.project.name}</p>
                         <p>{props.project.description}</p>
                         <p>{props.project.owner}</p>
                         <p>{props.project.balance.toString()}</p>
                         <p>{props.project.totalDonations.toString()}</p>
                     </div>
+                </div>
+                <div className="card-footer">
+                    2 days ago
                 </div>
             </div>
         </div>
