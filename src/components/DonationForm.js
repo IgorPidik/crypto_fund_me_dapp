0adf221dca6f7fc6b0621901879a1f179b0ac57d
Igor Pidik <igorPidik@gmail.com>
2021-11-19 20:01:12 +0100

    convert donations to wei

--- src/components/DonationForm.js
+++ src/components/DonationForm.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import {ethers} from "ethers";
 
 
 function DonationForm(props) {
+    const currencies = ['wei', 'eth']
+    const [selectedCurrency, setSelectedCurrency] = useState(currencies[0])
+    const [donationAmount, setDonationAmount] = useState("")
+
+    const options = currencies.map(currency => {
+        return <option key={currency} value={currency}>{currency}</option>
+    })
+
+    const handleDonatePressed = () => {
+        let wei
+        if (selectedCurrency === 'eth') {
+            wei = ethers.utils.parseEther(donationAmount)
+        } else {
+            wei = ethers.BigNumber.from(donationAmount)
+        }
+        console.log(wei)
+        console.log(wei.toString())
+    }
+
+    const handleInputChanged = (event) => {
+        let newValue = event.target.value
+        if (selectedCurrency === 'wei') {
+            newValue = newValue.replace(/\D/, '')
+        }
+       setDonationAmount(newValue)
+    }
+
     return (
         <div className="input-group black-input">
-            <input type="number" className="form-control black-input no-selection-effects" placeholder="0"/>
+            <input type="number" className="form-control black-input no-selection-effects" placeholder="0" value={donationAmount}
+                   onChange={handleInputChanged}/>
 
-            <select className="custom-select black-input no-selection-effects" id="inputGroupSelect04">
-                <option value="eth" selected>ETH</option>
-                <option value="wei">Wei</option>
+            <select className="black-input no-selection-effects"
+                    value={selectedCurrency} onChange={e => setSelectedCurrency(e.target.value)}>
+                {options}
             </select>
             <div className="input-group-append ms-2">
-                <button className="btn btn-success no-selection-effects" type="button">Donate</button>
+                <button className="btn btn-success no-selection-effects" type="button"
+                        disabled={!donationAmount}
+                        onClick={handleDonatePressed}>Donate
+                </button>
             </div>
         </div>
     );
