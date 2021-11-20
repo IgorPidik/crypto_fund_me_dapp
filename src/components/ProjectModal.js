36fd74e3d2bb753c1e92856d4334455c437fe7ff
Igor Pidik <igorPidik@gmail.com>
2021-11-20 17:51:01 +0100

    fix ui

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,10 +1,12 @@
 import React from "react";
-import {Modal} from "react-bootstrap";
+import {Col, Container, Modal, Row} from "react-bootstrap";
 import {ethers} from "ethers";
 import DonationForm from "./DonationForm";
+import {useWeb3React} from "@web3-react/core";
 
 
 function ProjectModal(props) {
+    const {account} = useWeb3React()
     const project = props.project
 
     if (!project) {
@@ -15,8 +17,8 @@ function ProjectModal(props) {
         props.onDonate(project.id, wei)
     }
 
-    // const isOwner = account === project.owner
-    const isOwner = true
+    const isOwner = account === project.owner
+
     return (
         <Modal show={true} onHide={props.onClose} centered contentClassName="bg-dark text-light">
             <Modal.Header>
@@ -29,17 +31,17 @@ function ProjectModal(props) {
                 <DonationForm onDonate={onHandleDonation}/>
             </Modal.Footer>
             <Modal.Footer className="justify-content-start">
-                {isOwner && <>
-                    <div className="d-flex flex-row flex-grow-1 align-items-center justify-content-between">
-                        <span>Current balance: {ethers.utils.formatEther(project.balance)} ETH</span>
-                        <a className="btn btn-link text-decoration-none p-0"
-                           onClick={() => props.onWithdraw(project.id)}>Withdraw</a>
-                    </div>
-                    <br/>
-                </>}
-                <span>Total funding: {ethers.utils.formatEther(project.totalDonations)} ETH</span>
-                <br/>
-                <span>Created by {project.owner}</span>
+                <Container fluid={true} className="p-0">
+                    {isOwner && <Row>
+                        <Col><span>Current balance: {ethers.utils.formatEther(project.balance)} ETH</span></Col>
+                        <Col className="col-auto">
+                            <a className="btn btn-link text-decoration-none p-0"
+                               onClick={() => props.onWithdraw(project.id)}>Withdraw</a>
+                        </Col>
+                    </Row>}
+                    <Row><span>Total funding: {ethers.utils.formatEther(project.totalDonations)} ETH</span></Row>
+                    <Row><span>Created by {project.owner}</span></Row>
+                </Container>
             </Modal.Footer>
         </Modal>
     );
