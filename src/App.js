e4d5e13bdca62f4e9f621075f6e444efebbd016f
Igor Pidik <igorPidik@gmail.com>
2021-11-20 16:50:11 +0100

    refactor and use react bootstrap elements

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import {injected} from "./connectors";
 import {serializeProject} from "./helpers";
 import ProjectCardView from "./components/ProjectCardView";
 import ProjectModal from "./components/ProjectModal";
+import CreateProjectModal from "./components/CreateProjectModal";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 
 function App() {
     const {active, account, library, activate, deactivate} = useWeb3React()
     const [contract, setContract] = useState(null)
     const [projects, setProjects] = useState([])
     const [modalProject, setModalProject] = useState(null)
+    const [showCreateProjectModal, setShowCreateProjectModal] = useState(false)
 
     const connect = async () => {
         try {
@@ -35,7 +38,7 @@ function App() {
     const loadContract = () => {
         axios.get('compiled_contracts/CryptoFundMe.json').then(response => {
             const abi = response.data['abi']
-            setContract(new ethers.Contract('0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab', abi, library))
+            setContract(new ethers.Contract('0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab', abi, library.getSigner(account)))
         });
     }
 
@@ -52,11 +55,11 @@ function App() {
 
     // automatically load contract when library (w3 provider) is loaded
     useEffect(() => {
-        if (library) {
+        if (library && account) {
             console.log('Loading contract')
             loadContract()
         }
-    }, [library])
+    }, [library, account])
 
     useEffect(() => {
         if (contract) {
@@ -84,6 +87,10 @@ function App() {
         console.log('donation', projectId, wei)
     }
 
+    const handleCreateProject = (title, description) => {
+        setShowCreateProjectModal(false)
+    }
+
     const projectViews = projects.map((project, index) => {
         return (<div className="col-3" key={project.id}>
             <ProjectCardView project={project} onClick={() => projectClicked(index)}/>
@@ -92,39 +99,37 @@ function App() {
 
     return (
         <div className="App">
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">CryptoFundMe</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"/>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarContent">
-                        <ul className="navbar-nav ms-auto">
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container fluid={true}>
+                    <Navbar.Brand href="#home">CryptoFundMe</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link onClick={() => setShowCreateProjectModal(true)}>Create project</Nav.Link>
+                        </Nav>
+                        <Nav>
                             {account ?
-                                <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button"
-                                       data-bs-toggle="dropdown" aria-expanded="false">{account}</a>
-                                    <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                        <li><a className="dropdown-item">My Projects</a></li>
-                                        <li className="dropdown-divider"/>
-                                        <li><a className="dropdown-item" onClick={disconnect}>Disconnect</a></li>
-                                    </ul>
-                                </li> :
-                                <a className="nav-link" onClick={connect}>Connect</a>
-                            }
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <div className="container-fluid">
+                                <NavDropdown title={account} align="end">
+                                    <NavDropdown.Item>My Projects</NavDropdown.Item>
+                                    <NavDropdown.Divider/>
+                                    <NavDropdown.Item onClick={disconnect}>Disconnect</NavDropdown.Item>
+                                </NavDropdown> :
+                                <Nav.Link onClick={connect}>Connect</Nav.Link>}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <Container fluid={true}>
                 <div className="row pt-3">
                     {projectViews}
                 </div>
-            </div>
+            </Container>
+
             <ProjectModal project={modalProject} onWithdraw={handleWithdraw} onDonate={handleDonation}
                           onClose={handleProjectModalClosed}/>
+            <CreateProjectModal show={showCreateProjectModal} onClose={() => setShowCreateProjectModal(false)}
+                                onCreateProject={handleCreateProject}/>
         </div>
     );
 }
